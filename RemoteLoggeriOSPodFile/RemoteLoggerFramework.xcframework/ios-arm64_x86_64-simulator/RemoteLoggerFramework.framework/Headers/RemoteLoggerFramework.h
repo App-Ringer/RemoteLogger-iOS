e38d21ffa7feb385a3ee4f84aa58c8003f4e880b
Ajay <ajay@appringer.com>
2022-07-11 10:43:18 +0530

    Initial commit

---
+++ RemoteLoggeriOSPodFile/RemoteLoggerFramework.xcframework/ios-arm64_x86_64-simulator/RemoteLoggerFramework.framework/Headers/RemoteLoggerFramework.h
@@ -0,0 +1,18 @@
+//
+//  RemoteLoggerFramework.h
+//  RemoteLoggerFramework
+//
+//  Created by iMac on 06/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RemoteLoggerFramework.
+FOUNDATION_EXPORT double RemoteLoggerFrameworkVersionNumber;
+
+//! Project version string for RemoteLoggerFramework.
+FOUNDATION_EXPORT const unsigned char RemoteLoggerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RemoteLoggerFramework/PublicHeader.h>
+
+
